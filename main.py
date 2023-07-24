ecd1839243445d5a6d425a81f4a69a6be051d8c9
Dolfour <rodolfohenriques23@gmail.com>
2023-07-24 19:56:58 -0300

    fix: realizando testes

--- main.py
+++ main.py
@@ -4,23 +4,11 @@ from management.product_handler import get_product_by_id, get_products_by_type, 
 
 
 def main():
-
-    found_product = get_product_by_id(25)
-    print(found_product)
-
-    print(get_products_by_type('drink'))
-
-    test_add = add_product(products,
-                      description='Concept of healthy breakfast with mesli',
-                      price=22.7, rating=2, title='Breakfast with muesli',
-                      type='dairy')
-    print(test_add)
-
-    table_1 = [{"_id": 1, "amount": 5}, {"_id": 19, "amount": 5}]
+    table_1 = [{"_id": 2, "amount": 8}, {"_id": 10, "amount": 4}]
     table_2 = [
-        {"_id": 10, "amount": 3},
-        {"_id": 20, "amount": 2},
-        {"_id": 21, "amount": 5},
+        {"_id": 11, "amount": 5},
+        {"_id": 22, "amount": 1},
+        {"_id": 31, "amount": 4},
     ]
 
     print(calculate_tab(table_1))
@@ -39,6 +27,16 @@ def main():
         "extra_key_2": "extra_value_2"
     }
 
+    print(add_product(products, **new_product))
+
+    new_product = {
+        "title": "X-Python",
+        "price": 5.0,
+        "rating": 5,
+        "description": "Sanduiche de Python",
+        "type": "fast-food",
+    }
+    print(add_product([], **new_product))
 
 if __name__ == "__main__":
     main()
